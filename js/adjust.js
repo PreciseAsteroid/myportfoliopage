ce1d03b9f387785a3eb27ec690a95def3160c163
Amir <baruch.amir@gmail.com>
2015-11-08 21:13:12 +0100

    many changes add favicon add logo moved the text below the icons in the carousel changed the contact links to work with the grid system

--- js/adjust.js
+++ js/adjust.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
     // Using jQuery's animate() method to add smooth page scroll
     // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
     $('html, body').animate({
-      scrollTop: $(hash).offset().top
+      scrollTop: $(hash).offset().top - 80 // scrolls to the section and moves down 75 pxs to componsate for the top bar
     }, 1000, function(){
 
       // Add hash (#) to URL when done scrolling (default click behavior)
